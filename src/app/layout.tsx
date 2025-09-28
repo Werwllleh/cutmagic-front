2126dc17eb492db6725873f33c0f5c5a3f4ed679
Alexey A <allvxm4@gmail.com>
2025-09-28 20:40:07 +0300

    fix ymetrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,24 +8,24 @@ import YandexMetrica from "@/components/yandex-metrica";
 
 export default function RootLayout({children,}: Readonly<{ children: ReactNode; }>) {
 
-  return (
-    <html lang="ru">
-    <head>
-      <link rel="canonical" href="https://cutmagic.ru/"/>
-      <meta name="yandex-verification" content="55c067d38c5b6b11"/>
-    </head>
-    <body>
-    <Providers>
-      <Header/>
-      <main>
-      <Suspense fallback={null}>
-          {children}
-          <YandexMetrica />
-        </Suspense>
-      </main>
-      <Footer/>
-    </Providers>
-    </body>
-    </html>
-  );
+    return (
+        <html lang="ru">
+        <head>
+            <link rel="canonical" href="https://cutmagic.ru/"/>
+            <meta name="yandex-verification" content="55c067d38c5b6b11"/>
+        </head>
+        <body>
+        <Providers>
+            <Header/>
+            <main>
+                <Suspense fallback={null}>
+                    {children}
+                    <YandexMetrica/>
+                </Suspense>
+            </main>
+            <Footer/>
+        </Providers>
+        </body>
+        </html>
+    );
 }
--- src/components/yandex-metrica.tsx
+++ src/components/yandex-metrica.tsx
@@ -22,36 +22,50 @@ const YandexMetrica = () => {
     }, []);
 
     useEffect(() => {
+
         if (typeof window !== "undefined" && window.ym) {
             const params = searchParams.toString();
             const url = base + pathName + (params ? "?" + params : "");
             ymReach("hit", url);
         }
+
     }, [pathName, searchParams]);
 
-    if (isDev) return null;
+    if (isDev) return;
 
     return (
-        <Script id="yandex-metrika" strategy="afterInteractive">
-            {`
-        (function(m,e,t,r,i,k,a){
-          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) { return; }
-          }
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-          k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(${Number(process.env.NEXT_PUBLIC_YMETRIKA)}, "init", {
-          defer: true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-        });
-      `}
-        </Script>
+        <>
+            <Script id="yandex-metrika" strategy="afterInteractive">
+                {`
+                (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                    if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+                ym(${Number(process.env.NEXT_PUBLIC_YMETRIKA)}, "init", {
+                  defer: true,
+                  clickmap:true,
+                  webvisor: true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+                });
+              `}
+            </Script>
+            <noscript>
+                <div>
+                    <img
+                        src={`https://mc.yandex.ru/watch/${Number(process.env.NEXT_PUBLIC_YMETRIKA)}`}
+                        style={{position: "absolute", left: "-9999px;"}}
+                        alt=""
+                    />
+                </div>
+            </noscript>
+        </>
     );
 };
 
