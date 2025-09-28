d13a1b5a7bece1c71471d18cbf0d63dd07d23f3c
Alexey A <allvxm4@gmail.com>
2025-09-28 20:59:33 +0300

    fix ymetrika

--- src/components/yandex-metrica.tsx
+++ src/components/yandex-metrica.tsx
@@ -12,15 +12,6 @@ const YandexMetrica = () => {
     const pathName = usePathname();
     const searchParams = useSearchParams();
 
-    const [isDev, setIsDev] = useState(true);
-
-
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setIsDev(window.location.hostname === "localhost");
-        }
-    }, []);
-
     useEffect(() => {
 
         if (typeof window !== "undefined" && window.ym) {
@@ -31,41 +22,42 @@ const YandexMetrica = () => {
 
     }, [pathName, searchParams]);
 
-    if (isDev) return;
-
     return (
-        <>
-            <Script id="yandex-metrika" strategy="afterInteractive">
-                {`
-                (function(m,e,t,r,i,k,a){
-                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  for (var j = 0; j < document.scripts.length; j++) {
-                    if (document.scripts[j].src === r) { return; }
-                  }
-                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        
-                ym(${Number(process.env.NEXT_PUBLIC_YMETRIKA)}, "init", {
-                  defer: true,
-                  clickmap:true,
-                  webvisor: true,
-                  trackLinks:true,
-                  accurateTrackBounce:true
-                });
-              `}
-            </Script>
-            <noscript>
-                <div>
-                    <img
-                        src={`https://mc.yandex.ru/watch/${Number(process.env.NEXT_PUBLIC_YMETRIKA)}`}
-                        style={{position: "absolute", left: "-9999px;"}}
-                        alt=""
-                    />
-                </div>
-            </noscript>
-        </>
+        process.env.NEXT_PUBLIC_START_MODE === "prod" && (
+            <>
+                <Script id="yandex-metrika" strategy="afterInteractive">
+                    {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+            ym(${Number(process.env.NEXT_PUBLIC_YMETRIKA)}, "init", {
+              defer: true,
+              clickmap:true,
+              webvisor: true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `}
+                </Script>
+                <noscript>
+                    <div>
+                        <img
+                            src={`https://mc.yandex.ru/watch/${Number(process.env.NEXT_PUBLIC_YMETRIKA)}`}
+                            style={{position: "absolute", left: "-9999px;"}}
+                            alt=""
+                        />
+                    </div>
+                </noscript>
+            </>
+        )
+
     );
 };
 
