4252d03e52ad8467f6078a20fae913be56884d6f
Alexey A <allvxm4@gmail.com>
2025-09-28 20:16:45 +0300

    fix ymetrika & add webmaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,12 @@ import {ReactNode, Suspense} from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import YandexMetrica from "@/components/yandex-metrica";
-import Head from "next/head";
 
 
 export default function RootLayout({children,}: Readonly<{ children: ReactNode; }>) {
 
-
   return (
     <html lang="ru">
-    <Head>
-      <meta name="yandex-verification" content="55c067d38c5b6b11"/>
-    </Head>
     <body>
     <Providers>
       <Header/>
